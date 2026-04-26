82ab0ce58f20162c7fb7a0756f7d0a1b4b364121
Austyn-Reichback <alreic03@louisville.edu>
2026-04-26 16:32:40 -0400

    Flask integrateion for LED and temperature(Testing Needed)

--- src/client/LED/led.ino
+++ src/client/LED/led.ino
@@ -1,12 +1,90 @@
-const int PIN = 0;
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "YOUR_WIFI";
+const char* password = "YOUR_PASS";
+
+String IP = ""; // You can set this to your local IP address
+String server = String("http://") + IP + ":5000";
+
+String hw_uuid = "";
+String hw_id = "";
+
+String getMac() {
+  return WiFi.macAddress();
+}
+
+bool doHandshake(String type) {
+  HTTPClient http;
+  http.begin(String(server) + "/handshake");
+  http.addHeader("Content-Type", "application/json");
+
+  StaticJsonDocument<200> doc;
+  doc["hw_id"] = hw_id;
+  doc["type"] = type;
+
+  String body;
+  serializeJson(doc, body);
+
+  int code = http.POST(body);
+
+  if (code == 200) {
+    String payload = http.getString();
+
+    StaticJsonDocument<200> res;
+    deserializeJson(res, payload);
+
+    hw_uuid = res["uuid"].as<String>();
+    http.end();
+    return true;
+  }
+
+  http.end();
+  return false;
+}
+
+const int LED_PIN = 0;
 
 void setup() {
-  pinMode(PIN, OUTPUT);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) delay(500);
+
+  hw_id = getMac();
+  doHandshake("control");
+
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(PIN, HIGH);
-  delay(1000);
-  digitalWrite(PIN, LOW);
-  delay(1000);
+  HTTPClient http;
+  http.begin(String(server) + "/control");
+  http.addHeader("Content-Type", "application/json");
+
+  StaticJsonDocument<200> doc;
+  doc["hw_uuid"] = hw_uuid;
+
+  JsonObject state = doc.createNestedObject("state");
+  state["led"] = digitalRead(LED_PIN) ? "on" : "off";
+
+  String body;
+  serializeJson(doc, body);
+
+  int code = http.POST(body);
+
+  if (code == 200) {
+    String payload = http.getString();
+
+    StaticJsonDocument<200> res;
+    deserializeJson(res, payload);
+
+    String desired = res["state"]["led"];
+
+    digitalWrite(LED_PIN, desired == "on" ? HIGH : LOW);
+  }
+
+  http.end();
+  delay(2000);
 }
--- src/client/temperature/temperature.ino
+++ src/client/temperature/temperature.ino
@@ -1,5 +1,53 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "YOUR_WIFI";
+const char* password = "YOUR_PASS";
+
+String IP = ""; // You can set this to your local IP address
+String server = String("http://") + IP + ":5000";
+
+String hw_uuid = "";
+String hw_id = "";
+
+String getMac() {
+  return WiFi.macAddress();
+}
+
+bool doHandshake(String type) {
+  HTTPClient http;
+  http.begin(String(server) + "/handshake");
+  http.addHeader("Content-Type", "application/json");
+
+  StaticJsonDocument<200> doc;
+  doc["hw_id"] = hw_id;
+  doc["type"] = type;
+
+  String body;
+  serializeJson(doc, body);
+
+  int code = http.POST(body);
+
+  if (code == 200) {
+    String payload = http.getString();
+
+    StaticJsonDocument<200> res;
+    deserializeJson(res, payload);
+
+    hw_uuid = res["uuid"].as<String>();
+    http.end();
+    return true;
+  }
+
+  http.end();
+  return false;
+}
+
+
 // Pin
 const int THERM_PIN = A0;
+
 
 // Constants
 const float SERIES_RESISTOR     = 10000.0; // 10k resistor
@@ -10,7 +58,17 @@ const float BETA_COEFFICIENT    = 3950.0;  // common value (check datasheet)
 // ADC
 const int ADC_MAX = 1023;
 
-void setup() { Serial.begin(115200); }
+void setup() { 
+  Serial.begin(115200); 
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) delay(500);
+  hw_id = getMac();
+  doHandshake("temperature");
+  
+  pinMode(THERM_PIN, INPUT);
+
+}
 
 void loop() {
   int adc = analogRead(THERM_PIN);
@@ -28,13 +86,23 @@ void loop() {
   tempC -= 273.15;                               // K → °C
 
   float tempF = tempC * 9.0 / 5.0 + 32.0;
+  HTTPClient http;
+  http.begin(String(server) + "/control");
+  http.addHeader("Content-Type", "application/json");
 
-  Serial.print("tempC: ");
-  Serial.print(tempC);
-  Serial.print(" C");
-  Serial.print(" (");
-  Serial.print(tempF);
-  Serial.println(" F)");
+  StaticJsonDocument<200> doc;
+  doc["hw_uuid"] = hw_uuid;
+  
+  JsonObject state = doc.createNestedObject("state");
+  state["temperature"] = tempC;
+
+  String body;
+  serializeJson(doc, body);
+  int code = http.POST(body);
+  if (code != 200) {
+    Serial.println("Failed to send temperature data");
+  }
+  http.end();
 
   delay(1000);
 }
